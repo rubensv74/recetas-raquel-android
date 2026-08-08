d4d6f73ed4d95f478168d096d33dec416b014da2
Rubén <seijoruben@gmail.com>
2026-08-08 20:49:38 +0200

    data: activate ingredient catalog v7

--- app/src/main/java/com/rmm/recetasraquel/data/catalog/IngredientCatalogAssetReader.kt
+++ app/src/main/java/com/rmm/recetasraquel/data/catalog/IngredientCatalogAssetReader.kt
@@ -69,6 +69,6 @@ class IngredientCatalogAssetReader(
     }
 
     companion object {
-        const val DEFAULT_VERSION_DIRECTORY = "ingredient-catalog/v6"
+        const val DEFAULT_VERSION_DIRECTORY = "ingredient-catalog/v7"
     }
 }
