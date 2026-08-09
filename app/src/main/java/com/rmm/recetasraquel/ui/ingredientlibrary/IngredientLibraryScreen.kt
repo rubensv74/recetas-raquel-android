37fa418e4346abd49f26a20b70eb6f7dd3923374
Rubén <seijoruben@gmail.com>
2026-08-09 20:32:50 +0200

    fix: import ingredient library column scope

--- app/src/main/java/com/rmm/recetasraquel/ui/ingredientlibrary/IngredientLibraryScreen.kt
+++ app/src/main/java/com/rmm/recetasraquel/ui/ingredientlibrary/IngredientLibraryScreen.kt
@@ -3,6 +3,7 @@ package com.rmm.recetasraquel.ui.ingredientlibrary
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
