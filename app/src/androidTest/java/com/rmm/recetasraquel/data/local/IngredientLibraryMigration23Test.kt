8e26e6a0af4afe202276298ccf30d26aad95a7ab
Rubén <seijoruben@gmail.com>
2026-08-13 19:54:17 +0200

    test: migrate historical databases through Room v7

--- app/src/androidTest/java/com/rmm/recetasraquel/data/local/IngredientLibraryMigration23Test.kt
+++ app/src/androidTest/java/com/rmm/recetasraquel/data/local/IngredientLibraryMigration23Test.kt
@@ -39,7 +39,7 @@ class IngredientLibraryMigration23Test {
                 IngredientLibraryMigrations.MIGRATION_2_3,
                 IngredientLibraryMigrations.MIGRATION_3_4,
                 IngredientLibraryMigrations.MIGRATION_4_5,
-                IngredientLibraryMigrations.MIGRATION_5_6,
+                IngredientLibraryMigrations.MIGRATION_5_6,\n                IngredientCompositionMigrations.MIGRATION_6_7,
             )
             .build()
 
@@ -104,7 +104,7 @@ class IngredientLibraryMigration23Test {
 
             db.query("PRAGMA user_version").use { cursor ->
                 assertTrue(cursor.moveToFirst())
-                assertEquals(6, cursor.getInt(0))
+                assertEquals(7, cursor.getInt(0))
             }
         } finally {
             database.close()
