d21bd81c49738883aeadeb06560836e85e365ef5
Rubén <seijoruben@gmail.com>
2026-08-13 19:54:15 +0200

    test: migrate historical databases through Room v7

--- app/src/androidTest/java/com/rmm/recetasraquel/data/local/RegulatoryExemptionMigration45Test.kt
+++ app/src/androidTest/java/com/rmm/recetasraquel/data/local/RegulatoryExemptionMigration45Test.kt
@@ -37,7 +37,7 @@ class RegulatoryExemptionMigration45Test {
         val database = Room.databaseBuilder(context, RecipeDatabase::class.java, TEST_DB)
             .addMigrations(
                 IngredientLibraryMigrations.MIGRATION_4_5,
-                IngredientLibraryMigrations.MIGRATION_5_6,
+                IngredientLibraryMigrations.MIGRATION_5_6,\n                IngredientCompositionMigrations.MIGRATION_6_7,
             )
             .build()
 
@@ -89,7 +89,7 @@ class RegulatoryExemptionMigration45Test {
 
             db.query("PRAGMA user_version").use { cursor ->
                 assertTrue(cursor.moveToFirst())
-                assertEquals(6, cursor.getInt(0))
+                assertEquals(7, cursor.getInt(0))
             }
         } finally {
             database.close()
