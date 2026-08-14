af7c09c4dae2121eb41eed1c8abcf69fa9e9b962
Rubén <seijoruben@gmail.com>
2026-08-14 22:36:24 +0200

    test: remove invalid Compose onNode import

--- app/src/androidTest/java/com/rmm/recetasraquel/ui/detail/RecipeSafetyPanelUiTest.kt
+++ app/src/androidTest/java/com/rmm/recetasraquel/ui/detail/RecipeSafetyPanelUiTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performScrollTo
